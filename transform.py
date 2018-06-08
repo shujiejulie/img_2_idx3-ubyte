c7f54d20eaca8ec86a276ccf055ed0da4650be24
FerdinandHan <shujie_ferdi@foxmail.com>
2018-06-08 13:48:54 +0800

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,13 +1,12 @@
 # _*_ coding:utf8 _*_
 
 # #####################################################################
-# 将图片数据库转化为与mnsit数据库格式相同的（.idx3-ubyte，.idx1-ubyte）文件
-# 方便后续实验操作
-# 注意图片数据库格式：
-# 根目录
-#   |___子目录1（目录名为标签值）
-#           |____图片1
-#           |____图片2
+# Convert the image database to the same format of file(.idx3-ubyte,.idx1-ubyte) as the mnsit database.
+# Make sure the original image database format is in the following form：
+# Root directory
+#   |___Subdirectory1（The name of subdirectory1 is a value of label）
+#           |____image1
+#           |____image2
 #           ...
 #   ...
 # #####################################################################
@@ -47,22 +46,8 @@ if __name__ == '__main__':
                              number_list[0],
                              number_list[1],
                              number_list[2])
-        '''
-        MMCBNU(6000)专用
-        makelabels.readLabel_idx3(paramDict['--name'] + 'labels.idx3-ubyte',
-                             labels_list,
-                             len(labels_list))
-
-        makeimages.readImage_idx3lab(paramDict['--name'] + 'data.idx3-ubyte',
-                             labels_list,
-                             paramDict['--img_dir'],
-                             number_list[0],
-                             number_list[1],
-                             number_list[2])
-        '''
 
     except getopt.GetoptError:
         print 'getopt error!'
         usage()
         sys.exit(1)
-
